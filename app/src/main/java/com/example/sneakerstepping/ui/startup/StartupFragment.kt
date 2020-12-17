da99314611c2ed0f41017ffc2e200eb76f96cf0c
abdessz <zakaria.ben.abdessadak@hva.nl>
2020-12-17 13:15:05 +0100

    new packages and name refactoring

--- app/src/main/java/com/example/sneakerstepping/ui/startup/StartupFragment.kt
+++ app/src/main/java/com/example/sneakerstepping/ui/startup/StartupFragment.kt
@@ -1,4 +1,4 @@
-package com.example.sneakerstepping.ui
+package com.example.sneakerstepping.ui.startup
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -12,14 +12,14 @@ import com.example.sneakerstepping.R
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class StartupFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.startup_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
