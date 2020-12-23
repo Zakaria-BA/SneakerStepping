2c5a8b0f7ebabe0cd9e496aa8594ab7b5d045156
abdessz <zakaria.ben.abdessadak@hva.nl>
2020-12-23 15:23:04 +0100

    loging in logic done

--- app/src/main/java/com/example/sneakerstepping/ui/startup/StartupFragment.kt
+++ app/src/main/java/com/example/sneakerstepping/ui/startup/StartupFragment.kt
@@ -1,19 +1,23 @@
 package com.example.sneakerstepping.ui.startup
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.sneakerstepping.R
+import com.example.sneakerstepping.models.User
 import com.example.sneakerstepping.ui.viewmodel.SneakerViewModel
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.register_fragment.*
 import kotlinx.android.synthetic.main.startup_fragment.*
 
 /**
@@ -36,9 +40,30 @@ class StartupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = findNavController()
+        initViews()
+    }
+
+    private fun initViews(){
         observeUser()
         createAccountButton.setOnClickListener { navigateUser() }
+        logInButton.setOnClickListener { logIn() }
     }
+
+    private fun logIn(){
+        viewModel.signIn(User(etEmail.text.toString(), etPassword.text.toString()), requireActivity())
+        updateUi(true)
+    }
+
+    private fun updateUi(logingIn: Boolean) {
+        if (logingIn) {
+            clLogin.isVisible = false
+            pb_loading_login.isVisible = true
+        } else {
+            clLogin.isVisible = true
+            pb_loading_login.isVisible = false
+        }
+    }
+
 
     private fun navigateUser(){
         navController.navigate(R.id.action_startupFragment_to_registerFragment)
@@ -48,7 +73,7 @@ class StartupFragment : Fragment() {
         viewModel.user.observe(viewLifecycleOwner, {
             if (it != null){
                 navController.navigate(R.id.action_startupFragment_to_homeFragment)
-            }
+            } else updateUi(false)
         })
     }
 
